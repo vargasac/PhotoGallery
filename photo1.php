8bf83dfad526628aaa095d6950be65d2372c8ffe
vargascharity <53499903+vargasac@users.noreply.github.com>
2019-08-13 09:25:16 +0800

    Update photo1.php

--- photo1.php
+++ photo1.php
@@ -9,8 +9,8 @@
 <h2>IMAGE 1</h2>
 <img src="https://scontent.fceb2-1.fna.fbcdn.net/v/t1.0-9/67176712_2142681049363197_2487135687606796288_n.jpg?_nc_cat=101&_nc_oc=AQmO9rflSYl4mNuxUnzSjHR8UdNcuS5J8mfWz0NluTY8x8D5oyZXUcyvR43QzQGTT2Y&_nc_ht=scontent.fceb2-1.fna&oh=b7b91fe66044ad457e558803bfa61e04&oe=5DC84956"/>
 <nav>
-<a href="HOME.html"<b>Back to Home</a>
-<a href="Image2.html"<b>Next Photo</a>
+<a href="index.php"<b>Back to Home</a>
+<a href="photo2.php"<b>Next Photo</a>
 </nav>
 <body style="background-color:Cyan;">
 </center>
