27e2d548b1b3804ef906ea7892f0ec665749466c
vargascharity <53499903+vargasac@users.noreply.github.com>
2019-08-13 09:26:23 +0800

    Update photo3.php

--- photo3.php
+++ photo3.php
@@ -9,7 +9,7 @@
 <h2>IMAGE 3</h2>
 <img src="https://scontent.fmnl13-1.fna.fbcdn.net/v/t1.0-9/67309770_10217841934854253_3646513043455606784_n.jpg?_nc_cat=108&_nc_oc=AQmq3qtQrZxgFT8FzdCRUTF13GlIQfAHUn3qZmQI81sswjArXYQoF_YVXII7HUWN2NE&_nc_ht=scontent.fmnl13-1.fna&oh=ff89e687a67c068a230554608c270b28&oe=5DCC8544"/>
 <nav>
-<a href="HOME.html"<b>Back to Home</a>
+<a href="index.php"<b>Back to Home</a>
 </nav>
 <body style="background-color:Cyan;">
 </center>
