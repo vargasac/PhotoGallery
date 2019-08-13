8ba3801fd131767ee13f82e83557d53ccc6d7cc3
vargascharity <53499903+vargasac@users.noreply.github.com>
2019-08-13 09:22:37 +0800

    Create photo2.php

---
+++ photo2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Image 2</title>
+</head>
+<body>
+<center>
+<h1>VargasC Photo Gallery</h1>
+<h2>IMAGE 2</h2>
+<img src="https://scontent.fceb2-2.fna.fbcdn.net/v/t1.0-9/67519839_2139004979730804_1708109670440239104_n.jpg?_nc_cat=107&_nc_oc=AQl49B4IAMSmE_xVm9PFkGUGT4_VH4VHRlqIPxAEzBFnYcXeLAndZcuGk6P5DLLgZ5w&_nc_ht=scontent.fceb2-2.fna&oh=3ce0a05993bb14d8b06f982aa9c2e6e6&oe=5DD65E02"/>
+<nav>
+<a href="HOME.html"<b>Back to Home</a>
+<a href="Image3.html"<b>Next Photo</a>
+</nav>
+<body style="background-color:Cyan;">
+</center>
+</body>
+</html>
