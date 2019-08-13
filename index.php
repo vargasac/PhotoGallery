1e92efa1d11326623bd30667dc0e1570371e1d6f
vargascharity <53499903+vargasac@users.noreply.github.com>
2019-08-13 09:21:28 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>HOME</title>
+</head>
+<body>
+<center>
+<h1>VargasC Photo Gallery</h1>
+<h2>List of Images</h2>
+<nav>
+<a href="Image1.html"<b>Image 1</a><br>
+<a href="Image2.html"<b>Image 2</a><br>
+<a href="Image3.html"<b>Image 3</a><br>
+</nav>
+<h4>My Images in College Life</h4>
+<p>"College life is that part of your life that you are going to relive in your memories till you breathe."</p><br>
+<h6>-coolnsmart</h6>
+<body style="background-color:BurlyWood;">
+</center>
+</body>
+</html>
