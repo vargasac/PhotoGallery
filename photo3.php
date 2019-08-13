1f5d35709c6b0cc6ae4f82f9d9ec5bcb81ff444f
vargascharity <53499903+vargasac@users.noreply.github.com>
2019-08-13 09:23:06 +0800

    Create photo3.php

---
+++ photo3.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Image 3</title>
+</head>
+<body>
+<center>
+<h1>VargasC Photo Gallery</h1>
+<h2>IMAGE 3</h2>
+<img src="https://scontent.fmnl13-1.fna.fbcdn.net/v/t1.0-9/67309770_10217841934854253_3646513043455606784_n.jpg?_nc_cat=108&_nc_oc=AQmq3qtQrZxgFT8FzdCRUTF13GlIQfAHUn3qZmQI81sswjArXYQoF_YVXII7HUWN2NE&_nc_ht=scontent.fmnl13-1.fna&oh=ff89e687a67c068a230554608c270b28&oe=5DCC8544"/>
+<nav>
+<a href="HOME.html"<b>Back to Home</a>
+</nav>
+<body style="background-color:Cyan;">
+</center>
+</body>
+</html>
