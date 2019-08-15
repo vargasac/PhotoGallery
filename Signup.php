6baf41dd0c8fac50688c71e80defa4a4cc24d4d3
vargascharity <53499903+vargasac@users.noreply.github.com>
2019-08-15 09:45:04 +0800

    Create Signup.php

---
+++ Signup.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Sign up</title>
+</head>
+<body>
+<body style="background-color:Gray;">
+<center>
+<h1>Sign Up</h1>
+<main>
+<form>
+Email Address:<input type="Email Address"required>
+<br><br>
+Confirm Email:<input type="Confirm Email"required>
+<br><br>
+Password:<input type="Password"required>
+<br><br>
+Confirm Password:<input type="Confirm Password"required>
+<br><br>
+<button><a href="Welcome.php"<b>Sign Up</a></button>
+</center>
+</form>
+
+</body>
+</html>
