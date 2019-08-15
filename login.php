a57124b8ca40973427d3919478d9245c2d9dbb49
vargascharity <53499903+vargasac@users.noreply.github.com>
2019-08-15 09:39:36 +0800

    Create login.php

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<head>
+<title>Login Page</title>
+</head>
+<body>
+<h1 align="center">Login</h1>
+<main>
+<form>
+<center>
+username:<input type="username"required>
+<br><br>
+password:<input type="password"required>
+<br><br><br>
+<button><a href="index.php"<b>Sign In</a></button>
+<br><br>
+<a href="Resetpassword.php"<b>Reset Password</a>
+<br><br>
+<a href="Signup.php"<b>Sign Up</a><br>
+</form>
+
+</body>
+</html>
