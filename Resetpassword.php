ccce826a73da98daaae19a22be9ac5b599821e66
vargascharity <53499903+vargasac@users.noreply.github.com>
2019-08-15 09:43:25 +0800

    Create Resetpassword.php

---
+++ Resetpassword.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<head>
+<title>Reset Password</title>
+</head>
+<body>
+<body style="background-color:Gray;">
+<h1 align="center">Reset Password</h1>
+<center>
+Enter Email Address:<input type="Email address"required>
+<br>
+<br>
+<button>Reset Password</button>
+<br><br>
+</center>
+
+</body>
+</htm>
