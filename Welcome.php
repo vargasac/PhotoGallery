d70bfc6a87c6f6e17e2c0f9d00a237beca692c2b
vargascharity <53499903+vargasac@users.noreply.github.com>
2019-08-15 09:50:23 +0800

    Create Welcome.php

---
+++ Welcome.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome</title>
+</head>
+<body>
+<body style="background-color:Green;">
+<center>
+<h1>WELCOME TO MY PHOTO GALLERY</h1>
+<a href="Photogallery.php">Proceed to Photo Gallery</a>
+</center>
+</body>
+</html>
